8cd22230f9a46b27b7ad63a1cef96ef2496e9e77
Favyen Bastani <favyenb@allenai.org>
2024-05-17 08:43:58 -0700

    fixed satlas

--- torchgeo/trainers/segmentation_satlas.py
+++ torchgeo/trainers/segmentation_satlas.py
@@ -1,63 +1,40 @@
 import satlaspretrain_models
+import torch
 from torch import Tensor
 
 from torchgeo.trainers import SemanticSegmentationTask
 
 
+class SatlasSegmentationModel(torch.nn.Module):
+    def __init__(self, model_identifier, fpn, num_classes):
+        super().__init__()
+        weights_manager = satlaspretrain_models.Weights()
+        self.model = weights_manager.get_pretrained_model(
+            model_identifier=model_identifier,
+            fpn=fpn,
+        )
+        self.head = torch.nn.Sequential(
+            torch.nn.Conv2d(128, 128, 3, padding=1),
+            torch.nn.ReLU(inplace=True),
+            torch.nn.Conv2d(128, num_classes, 3, padding=1),
+        )
+
+    def forward(self, x):
+        feature_maps = self.model(x)
+        assert len(feature_maps) == 5 # 1 upsampled at beginning + 4 FPN from before the upsampling
+        return self.head(feature_maps[0])
+
+
 class SatlasSemanticSegmentationTask(SemanticSegmentationTask):
     def __init__(self, model_identifier: str, fpn: bool = True, pretrained: bool = True, *args, **kwargs):
-        self.weights_manager = satlaspretrain_models.Weights()
         self.model_identifier = model_identifier
         self.pretrained = pretrained
         self.fpn = fpn
         super().__init__(*args, **kwargs)
 
     def configure_models(self):
-        self.model = self.weights_manager.get_pretrained_model(
-            model_identifier=self.model_identifier,
-            fpn=self.fpn,
-            head=satlaspretrain_models.Head.SEGMENT,
-            num_categories=self.hparams["num_classes"]
-        )
+        self.model = SatlasSegmentationModel(self.model_identifier, self.fpn, self.hparams["num_classes"])
 
         if self.hparams["freeze_backbone"]:
-            for param in self.model.backbone.parameters():
+            for param in self.model.model.backbone.parameters():
                 param.requires_grad = False
-
-    def training_step(self, batch, batch_idx):
-        """Compute the training loss and additional metrics.
-
-        Args:
-            batch: The output of your DataLoader.
-            batch_idx: Integer displaying index of this batch.
-            dataloader_idx: Index of the current dataloader.
-
-        Returns:
-            The loss tensor.
-        """
-        x = batch["image"]
-        y = batch["mask"]
-        batch_size = x.shape[0]
-        y_hat, loss = self(x)  # don't really use loss computed by satlaspretrain package
-        loss: Tensor = self.criterion(y_hat, y)
-        self.log("train_loss", loss, batch_size=batch_size)
-        self.train_metrics(y_hat, y)
-        self.log_dict(self.train_metrics, batch_size=batch_size)
-        return loss
-
-    def validation_step(self, batch, batch_idx):
-        """Compute the validation loss and additional metrics.
-
-        Args:
-            batch: The output of your DataLoader.
-            batch_idx: Integer displaying index of this batch.
-            dataloader_idx: Index of the current dataloader.
-        """
-        x = batch["image"]
-        y = batch["mask"]
-        batch_size = x.shape[0]
-        y_hat, loss = self(x)  # don't really use loss computed by satlaspretrain package
-        loss = self.criterion(y_hat, y)
-        self.log("val_loss", loss, batch_size=batch_size)
-        self.val_metrics(y_hat, y)
-        self.log_dict(self.val_metrics, batch_size=batch_size)
